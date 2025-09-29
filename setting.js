8587d12fd14773993e83101b927e99b3c713c53e
rynzz <samyonosamyoni@gmail.com>
2025-09-29 12:35:12 +0700

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,12 @@
+// settings.js
+const settings = {
+  title: "tes",
+  adminPassword: "tesadmin",
+  userPassword: "tesuser",
+  adminWa: "6281246041489",
+  githubToken: "",
+  repo: "",
+  owner: "",
+  fileDb: "",
+  background: "https://files.catbox.moe/u2amkq.jpg"
+};
